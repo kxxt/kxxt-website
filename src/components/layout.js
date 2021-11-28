93a6f004201d8e9dd0478bb6360a0e25176dd52a
kxxt <rsworktech@outlook.com>
2021-11-28 20:02:36 +0800

    Customize layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,24 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import Navbar from "./navbar"
+import Footer from "./footer"
+import { Helmet } from "react-helmet"
 
-const Layout = ({ location, title, children }) => {
+const Layout = ({ location, children, hero = null }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
 
   return (
-    <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
-    </div>
+    <>
+      <Helmet>
+        <body className="has-navbar-fixed-top-desktop" />
+      </Helmet>
+      <Navbar title="kxxt" />
+      {hero}
+      <div id="main-container" className="container" data-is-root-path={isRootPath}>
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
   )
 }
 
