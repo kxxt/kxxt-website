679cb86d0e90c4730ec5fb32470d39826fa9a865
kxxt <rsworktech@outlook.com>
2021-11-28 22:01:30 +0800

    Use BlogSummary.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import ReactTyped from "react-typed"
 import { StaticImage } from "gatsby-plugin-image"
+import BlogSummary from "../components/blog-summary"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -86,32 +87,9 @@ const BlogIndex = ({ data, location }) => {
 
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
           return (
             <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
+              <BlogSummary post={post} />
             </li>
           )
         })}
@@ -139,6 +117,7 @@ export const pageQuery = graphql`
                     date(formatString: "MMMM DD, YYYY")
                     title
                     description
+                    tags
                 }
             }
         }
