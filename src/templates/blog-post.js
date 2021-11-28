e1f4e2ea842add56bb44a58ef88b4c642e24ace7
kxxt <rsworktech@outlook.com>
2021-11-28 22:07:08 +0800

    Add content class to BlogPost.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -17,7 +17,7 @@ const BlogPostTemplate = ({ data, location }) => {
         description={post.frontmatter.description || post.excerpt}
       />
       <article
-        className="blog-post"
+        className="blog-post content"
         itemScope
         itemType="http://schema.org/Article"
       >
