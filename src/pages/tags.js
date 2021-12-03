0a24aa396b7e2205df6776c86ea3b0dbf04c1a37
kxxt <rsworktech@outlook.com>
2021-12-03 15:45:12 +0800

    Add tags page.

---
+++ src/pages/tags.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Tags from "../components/tags"
+
+const TagsPage = ({ data, location }) => {
+  const title = "Tags"
+  const tags = data.allMdx.group
+  return (
+    <Layout location={location} title={title}>
+      <Seo title={title} />
+      <h1 className="title">All Tags</h1>
+      <Tags tags={tags} withCount={true} />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+    query {
+        allMdx {
+            group(field: frontmatter___tags) {
+                tag: fieldValue
+                totalCount
+            }
+        }
+    }`
+
+export default TagsPage
