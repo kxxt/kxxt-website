307a74539d4b7ca68e67f785c1c86a886ce7f03e
kxxt <rsworktech@outlook.com>
2021-12-03 17:50:57 +0800

    Add blogs page.

---
+++ src/pages/blogs.js
@@ -0,0 +1,44 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import BlogSummaryList from "../components/blog-summary-list"
+
+const BlogsPage = ({ data, location }) => {
+  const posts = data.allMdx.nodes
+  const title = "Blogs"
+
+  return (
+    <Layout location={location} title={title}>
+      <Seo title={title} />
+      <h1 className="title">{title}</h1>
+      <BlogSummaryList posts={posts} />
+    </Layout>
+  )
+}
+
+export default BlogsPage
+
+export const pageQuery = graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+        allMdx(sort: {fields: [frontmatter___date], order: DESC}) {
+            nodes {
+                excerpt
+                slug
+                timeToRead
+                frontmatter {
+                    date(formatString: "MMMM DD, YYYY")
+                    title
+                    description
+                    tags
+                }
+            }
+        }
+    }
+`
