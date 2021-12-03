046fbb2893106a6dce888a4b49b065a7a1d96dc2
kxxt <rsworktech@outlook.com>
2021-12-03 15:44:59 +0800

    Separate TagLink from Tags and add more functionalities.

--- src/components/tags.js
+++ src/components/tags.js
@@ -1,19 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const Tags = ({ tags }) => {
+
+const TagLink = ({ tag, totalCount = 0, fontSize }) => {
   return (
-    <div className="tags">
-      {tags.map(tag => (
-        <Link
-          key={tag}
+    <Link key={tag}
           to={`/tags/${tag}/`}
           className="tag is-primary"
-          itemProp="keywords"
-        >
-          {tag}
-        </Link>
-      ))}
+          style={{ fontSize }}
+          itemProp="keywords">
+      {totalCount ? `${tag} (${totalCount})` : tag}
+    </Link>
+  )
+}
+
+const Tags = ({ tags, withCount = false, fontSize = "14px" }) => {
+
+  return (
+    <div className="tags">
+      {
+        withCount
+          ? tags.map(({ tag, totalCount }) => (
+            <TagLink key={tag} tag={tag} totalCount={totalCount} fontSize={fontSize} />
+          ))
+          : tags.map(tag => (
+            <TagLink key={tag} tag={tag} fontSize={fontSize} />
+          ))
+      }
     </div>
   )
 }
