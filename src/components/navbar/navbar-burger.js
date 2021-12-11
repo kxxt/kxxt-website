e8d66524d45dbe5a15e5270b243e0d360bd221af
kxxt <rsworktech@outlook.com>
2021-12-11 23:13:46 +0800

    Disable jsx-a11y/anchor-is-valid for burger button

--- src/components/navbar/navbar-burger.js
+++ src/components/navbar/navbar-burger.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 const NavbarBurger = ({ toggle }) => (
+  // eslint-disable-next-line jsx-a11y/anchor-is-valid
   <a href="#" role="button" onClick={toggle} data-target="navbar-menu" className="navbar-burger" aria-label="menu"
      aria-expanded="false">
     <span aria-hidden="true" />
