ed028f620522c585c678b82e5cf204c5f86c9928
kxxt <rsworktech@outlook.com>
2021-12-24 18:34:19 +0800

    Use local image for CC-BY-NC-SA license picture.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { faGithub, faTelegram } from "@fortawesome/free-brands-svg-icons"
+import { StaticImage } from "gatsby-plugin-image"
 import { faAt, faRss } from "@fortawesome/free-solid-svg-icons"
 import IconText from "../icon-text"
 import * as styles from "./footer.module.scss"
@@ -16,9 +17,9 @@ const Footer = () => {
 
             This is kxxt's personal website.
             <br />
-            <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><img
+            <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><StaticImage
               alt="Creative Commons License" style={{ borderWidth: 0, marginTop: "1rem", width: 88, height: 31 }}
-              src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />Unless specified, the contents of
+              src="../../images/cc-by-nc-sa-4.png" /></a><br />Unless specified, the contents of
             this website are licensed under a <a
             rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons
             Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
