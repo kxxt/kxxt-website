e7fad5cb8fb010d7b34852519f436e62af67d1ed
kxxt <rsworktech@outlook.com>
2021-12-25 10:44:06 +0800

    Switch to gatsby-plugin-google-gtag from gatsby-plugin-google-analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,11 +87,22 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: `297698399`,
-        respectDNT: true,
-        head: false,
+        trackingIds: [
+          "G-FLBVN7RJJQ", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          respectDNT: true,
+        },
       },
     },
     {
