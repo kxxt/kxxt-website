c6583c3c92fd6642d903e6debd0251390e65f9c6
kxxt <rsworktech@outlook.com>
2021-12-25 16:35:51 +0800

    Add sitemap.

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,20 +90,20 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-          "G-FLBVN7RJJQ", // Google Analytics / GA
+          "G-FLBVN7RJJQ" // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
-          anonymize_ip: true,
+          anonymize_ip: true
         },
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: false,
-          respectDNT: true,
-        },
-      },
+          respectDNT: true
+        }
+      }
     },
     {
       resolve: `gatsby-plugin-feed`,
@@ -154,6 +154,13 @@ module.exports = {
             title: "kxxt's rss feed"
           }
         ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap`,
+        createLinkInHead: true
       }
     },
     {
