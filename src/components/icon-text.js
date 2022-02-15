45cad0eac562291b6f7cceed75a5c7394f4e7756
kxxt <rsworktech@outlook.com>
2022-02-15 16:02:42 +0800

    fix: large icon.

--- src/components/icon-text.js
+++ src/components/icon-text.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import * as styles from "./icon-text.module.scss"
 
 const IconText = ({ icon, children, color = null }) => {
   return (
-    <span className="icon-text">
+    <span className={`icon-text ${styles.iconText}`}>
       <FontAwesomeIcon color={color} className="icon" icon={icon} />
       <span>{children}</span>
     </span>
