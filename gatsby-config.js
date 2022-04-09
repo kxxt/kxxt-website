3878dc34f76a7961c427034667c5e02e45cbf390
kxxt <rsworktech@outlook.com>
2022-04-09 10:53:07 +0800

    content: Update site url.

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       summary: `A student studying Data Science and Artificial Intelligence. Major Language: C# and Python. My personal chinese TG channel: https://t.me/kxxtchannel`,
     },
     description: `kxxt's personal website`,
-    siteUrl: `https://kxxt.vercel.app/`,
+    siteUrl: `https://kxxt.me`,
   },
   plugins: [
     // {
