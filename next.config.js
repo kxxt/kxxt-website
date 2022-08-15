45b429b777c94e29e7f960b825f807f74fd2643d
kxxt <rsworktech@outlook.com>
2022-08-15 21:39:25 +0800

    feat: redir /blog to /blogs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,25 @@
 module.exports = {
-	webpack: (config) => {
-		// camel-case style names from css modules
-		config.module.rules
-			.find(({oneOf}) => !!oneOf).oneOf
-			.filter(({use}) => JSON.stringify(use)?.includes('css-loader'))
-			.reduce((acc, {use}) => acc.concat(use), [])
-			.forEach(({options}) => {
-				if (options.modules) {
-					options.modules.exportLocalsConvention = 'camelCase';
-				}
-			});
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: "/blogs",
+        permanent: true,
+      },
+    ]
+  },
+  webpack: config => {
+    // camel-case style names from css modules
+    config.module.rules
+      .find(({ oneOf }) => !!oneOf)
+      .oneOf.filter(({ use }) => JSON.stringify(use)?.includes("css-loader"))
+      .reduce((acc, { use }) => acc.concat(use), [])
+      .forEach(({ options }) => {
+        if (options.modules) {
+          options.modules.exportLocalsConvention = "camelCase"
+        }
+      })
 
-		return config;
-	},
-};
+    return config
+  },
+}
