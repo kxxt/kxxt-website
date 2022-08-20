fa22d17c7ef70f4fc79a3f1d6400d851d075d1be
kxxt <rsworktech@outlook.com>
2022-08-21 07:37:24 +0800

    next-image-export-optimizer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  trailingSlash: true,
+  images: {
+    loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  env: {
+    nextImageExportOptimizer_imageFolderPath: "public/images",
+    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
+  },
   async redirects() {
     return [
       {
