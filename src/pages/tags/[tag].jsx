0bd1b77235c5f29fe7cb5df33222535b22d0c740
kxxt <rsworktech@outlook.com>
2022-08-21 07:41:38 +0800

    make tag page template build

--- src/pages/tags/[tag].jsx
+++ src/pages/tags/[tag].jsx
@@ -1,17 +1,17 @@
-import Layout from "../../components/layout"
-import Seo from "../../components/seo"
-import BlogSummaryList from "../../components/blog-summary/blog-summary-list"
+import Layout from "@/components/layout"
+import Seo from "@/components/seo"
+import BlogSummaryList from "@/components/blog-summary/blog-summary-list"
 
-const TagPage = ({ data, location, pageContext }) => {
-  const title = `Tag ${pageContext.tag}`
-  const posts = data.allMdx.nodes
+const TagPage = ({ tag, cnt }) => {
+  const title = `Tag ${tag}`
+  const posts = []
   return (
-    <Layout location={location} title={title}>
+    <Layout title={title}>
       <Seo title={title} />
-      <h1 className="title">Tag: {pageContext.tag}</h1>
+      <h1 className="title">Tag: {tag}</h1>
       <p className="subtitle is-5">
-        Found {pageContext.totalCount} page
-        {pageContext.totalCount > 1 ? "s" : ""} with tag &quot;{pageContext.tag}
+        Found {"TODO"} page
+        {cnt > 1 ? "s" : ""} with tag &quot;{tag}
         &quot;
       </p>
       <BlogSummaryList posts={posts} />
