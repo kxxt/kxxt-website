9a9bef7b7287ef6a88b600de9eea1e6c826393f9
kxxt <rsworktech@outlook.com>
2022-11-04 23:39:20 +0800

    friends page

--- src/pages/friends.js
+++ src/pages/friends.js
@@ -1,15 +1,35 @@
 import React from "react"
 
+import { StaticImage } from "gatsby-plugin-image"
+
 import FriendCard from "@/components/friend-card"
 import Layout from "@/components/layout/layout"
 import Seo from "@/components/seo"
 
-const FriendPage = ({ data, location }) => {
+import * as styles from "./friends.module.scss"
+
+const FriendPage = ({ location }) => {
   const title = "Friends"
   return (
     <Layout location={location} title={title}>
       <Seo title={title} />
       <h1 className="title">{title}</h1>
+      <div className={`${styles.friendContainer} tile is-ancestor`}>
+        <FriendCard
+          name="kxxt"
+          github="kxxt"
+          description="Undergraduate & Developer"
+          link="/"
+          icon={
+            <StaticImage
+              src="../images/friends/kxxt.png"
+              alt="kxxt"
+              width={48}
+              height={48}
+            />
+          }
+        />
+      </div>
     </Layout>
   )
 }
