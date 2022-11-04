c76efe8ed788b7facfb7201aaa253e7b25470803
kxxt <rsworktech@outlook.com>
2022-11-04 23:07:29 +0800

    friend card component

--- src/components/friend-card.jsx
+++ src/components/friend-card.jsx
@@ -1,12 +1,32 @@
 import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import * as styles from "./friend-card.module.scss"
 
-export default function FriendCard({ icon, name, description }) {
+export default function FriendCard({
+  icon,
+  name,
+  description,
+  className,
+  github,
+}) {
   return (
-    <div class="card">
-      <StaticImage src="../images/friends/kxxt.png" />
-      <h3>{name}</h3>
-      <p>{description}</p>
+    <div class={`card ${styles.card} ${className}`}>
+      <div class={`card-content`}>
+        {/* Gatsby doesn't allow me to forward src to StaticImage. 
+            But I don't want to use a dynamic image here either. 
+            So this is a workaround: let the caller pass a StaticImage to us.*/}
+        <div class={`media ${styles.media}`}>
+          <div class="media-left">
+            <figure class="image is-48x48">{icon}</figure>
+          </div>
+          <div class="media-content">
+            <p class={`title is-4 ${styles.name}`}>{name}</p>
+            <p class="subtitle is-6">
+              <a href={`https://github.com/${github}`}>@{github}</a>
+            </p>
+          </div>
+        </div>
+        <div class="content">{description}</div>
+      </div>
     </div>
   )
 }
