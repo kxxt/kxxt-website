7b5add61fdd7b585cc570df7c5d6f54016088ab7
kxxt <rsworktech@outlook.com>
2022-11-08 10:28:18 +0800

    feat: MDX page layout component

---
+++ src/components/layout/mdx.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "."
+
+export default function MDXPageLayout({ children, ...props }) {
+  return (
+    <Layout {...props}>
+      <article
+        className="blog-post content"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        {children}
+      </article>
+    </Layout>
+  )
+}
