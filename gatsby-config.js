004e4f88d341e6afff74564e9b1d6916c36e52ea
kxxt <rsworktech@outlook.com>
2022-11-08 09:59:10 +0800

    feat: enable mdx in pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,6 +106,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/src/pages`,
+        name: `pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         path: `${__dirname}/content/blog`,
         name: `blog`,
       },
