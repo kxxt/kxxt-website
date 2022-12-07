a84885fca1c799ca19e0330df2496619092a348a
kxxt <rsworktech@outlook.com>
2022-12-07 22:32:33 +0800

    update 404 title

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ import Seo from "@/components/seo"
 const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location}>
-      <Seo title="404: Not Found" />
+      <Seo title="404 Not Found" />
       <h1 className="title">404: Not Found</h1>
       <p className="content">
         You just hit a route that doesn&#39;t exist... the sadness.
