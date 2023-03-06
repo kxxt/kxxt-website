fa2a5b1f9d4f35083431e08ad10d990d5bbc5026
kxxt <rsworktech@outlook.com>
2023-03-06 13:57:45 +0800

    fix: style of project card collection

--- src/components/project-card/project-cards.jsx
+++ src/components/project-card/project-cards.jsx
@@ -2,5 +2,5 @@ import React from "react"
 import * as styles from "./project-cards.module.scss"
 
 export default function ProjectCards({ children }) {
-  return <div className={styles.cardList}>${children}</div>
+  return <div className={styles.cardList}>{children}</div>
 }
