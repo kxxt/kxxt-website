042a3dc15e232ea8afbe949b83b8b4c0831a95fa
kxxt <rsworktech@outlook.com>
2023-04-29 10:30:19 +0800

    fix: remove react helmet related dependencies

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -256,7 +256,6 @@ export default {
         icon: `src/images/profile-pic.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
