9859a83205347b26ed91fbd3491cdae2f7d22828
Levi Zim (kxxt) <rsworktech@outlook.com>
2024-09-08 22:10:34 +0800

    content(blog): riscv chromium cross compilation

--- src/components/mdx/PR.jsx
+++ src/components/mdx/PR.jsx
@@ -2,12 +2,12 @@ import React from "react"
 import IconLink from "../icon-link"
 import { faCodePullRequest } from "@fortawesome/free-solid-svg-icons"
 
-export default function PR({ number, open, repo, full, half }) {
+export default function PR({ number, open, repo, full, half, title }) {
   return (
     <IconLink
       icon={faCodePullRequest}
       href={`https://github.com/${repo}/pull/${number}`}
       color={open ? "#1a7f37" : "#8250df"}
-    >{`${full ? repo : half ? repo.split("/")[1] : ""}#${number}`}</IconLink>
+    >{`${full ? repo : half ? repo.split("/")[1] : ""}#${number}`}{title ? ` ${title}` : null}</IconLink>
   )
 }
